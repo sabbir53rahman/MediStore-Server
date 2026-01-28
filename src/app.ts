4abe3625923257a16b267f98db91888e6cfa4aef
sabbir53rahman <sabbir53rahman@gmail.com>
2026-01-28 08:05:26 +0600

    create lib/auth.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,20 @@
 import express, { Application } from "express";
 import { medicineRouter } from "./modules/medicine/medicine.router";
 import { categoryRouter } from "./modules/category/category.router";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
+import cors from "cors";
 
 const app: Application = express();
+
+app.use(
+  cors({
+    origin: process.env.APP_URL || "http://localhost:4000",
+    credentials: true,
+  })
+);
+
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.use(express.json());
 
